144f5fa9e010fa0f6aec57a17e36d24485123598
Andrew <522988922@qq.com>
2019-08-16 10:37:17 +0800

    初始化新题目

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,13 @@
-import Utils.Sout;
-import org.apache.log4j.Logger;
-
-import static LeeCode.AddTwoNumbers.ListNode;
-import static LeeCode.AddTwoNumbers.addTwoNumbers;
+import LeeCode.LengthOfLongestSubstring;
 
 class Main {
 //    private static Logger logger = Logger.getLogger(Main.class);
 
-    /***
-     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
-     * 输出：7 -> 0 -> 8
-     * 原因：342 + 465 = 807
-     */
     public static void main(String[] args) {
         //  logger.info("helloworld");
 
-        ListNode listNode1 = new ListNode(2);
-        ListNode listNode11 = new ListNode(4);
-        ListNode listNode111 = new ListNode(3);
-        listNode1.setNext(listNode11);
-        listNode11.setNext(listNode111);
-
-        ListNode listNode2 = new ListNode(5);
-        ListNode listNode22 = new ListNode(6);
-        ListNode listNode222 = new ListNode(4);
-        listNode2.setNext(listNode22);
-        listNode22.setNext(listNode222);
-
-        Sout.soutNum(addTwoNumbers(listNode1, listNode2));
-        //0没了？
+        System.out.println(LengthOfLongestSubstring.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(LengthOfLongestSubstring.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(LengthOfLongestSubstring.lengthOfLongestSubstring("pwwkew"));
     }
 }
