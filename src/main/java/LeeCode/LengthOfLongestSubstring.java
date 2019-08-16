054b71b17d92fc24ec787a3e2c1ba0ef2647109a
Andrew <522988922@qq.com>
2019-08-16 11:14:12 +0800

    三个案例都过，暴力发，开始为修改滑动窗口

--- src/main/java/LeeCode/LengthOfLongestSubstring.java
+++ src/main/java/LeeCode/LengthOfLongestSubstring.java
@@ -1,5 +1,7 @@
 package LeeCode;
 
+import java.util.HashMap;
+
 public class LengthOfLongestSubstring {
     /**
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -9,11 +11,13 @@ public class LengthOfLongestSubstring {
      * 输入: "abcabcbb"
      * 输出: 3
      * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+     * <p>
      * 示例 2:
      * <p>
      * 输入: "bbbbb"
      * 输出: 1
      * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+     * <p>
      * 示例 3:
      * <p>
      * 输入: "pwwkew"
@@ -24,8 +28,25 @@ public class LengthOfLongestSubstring {
 
     public static int lengthOfLongestSubstring(String s) {
         //滑动窗口，利用hashmap去重
+        if (s.isEmpty()) return 0;
 
+        int slen = s.length();
+        int max = 0;
 
-        return 1;
+        for (int i = 0; i < slen; i++) {
+            HashMap<Character, String> hashMap = new HashMap<Character, String>();
+            int flag = i;
+            for (int j = i; j < slen; j++, flag++) {
+                if (hashMap.containsKey(s.charAt(j))) {
+                    max = Math.max(max, (flag - j));
+                    break;
+                } else {
+                    hashMap.put(s.charAt(j), "");
+                    flag++;
+                }
+            }
+
+        }
+        return max;
     }
 }
