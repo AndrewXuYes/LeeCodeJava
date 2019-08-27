e44dfd74ceb5c837f107c828330b0a4ca1ddf961
Andrew <522988922@qq.com>
2019-08-27 19:34:46 +0800

    mulString

--- src/main/java/LeeCode/_43_multiply.java
+++ src/main/java/LeeCode/_43_multiply.java
@@ -19,8 +19,41 @@ public class _43_multiply {
         return bigDecimal3.toString();
     }
 
+    public static String multiply2(String num1, String num2) {
+        return String.valueOf(Long.parseLong(num1) * Long.parseLong(num2));
+    }
+
+    public static String multiply3(String num1, String num2) {
+
+        int num1len = num1.length();
+        int num2len = num2.length();
+
+        if (num1len == 0 || num2len == 0) return "";
+
+        int[] mul = new int[num1len + num2len];
+        StringBuilder stringBuilder = new StringBuilder("");
+
+        for (int i = num1len - 1; i >= 0; i--) {
+            for (int j = num2len - 1; j >= 0; j--) {
+                int bitmul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                bitmul += mul[i + j + 1]; // 先加低位判断是否有新的进位
+                mul[i + j] += bitmul / 10;
+                mul[i + j + 1] = bitmul % 10;
+            }
+        }
+
+        int i = 0;
+        while (i < mul.length - 1 && mul[i] == 0) i++;
+        while (i < mul.length) stringBuilder.append(mul[i++]);
+
+        return stringBuilder.toString();
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(multiply("12", "32"));
+        System.out.println("multiply==" + multiply("12", "12"));
+        System.out.println("multiply2==" + multiply2("12", "12"));
+        System.out.println("multiply3==" + multiply3("12", "12"));
+
     }
 }
