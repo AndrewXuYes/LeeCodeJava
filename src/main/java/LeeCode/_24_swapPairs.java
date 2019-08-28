cd45c1b26a65da10bb7fb469151b3b234c458ef4
Andrew <522988922@qq.com>
2019-08-28 11:03:43 +0800

    leecode

--- src/main/java/LeeCode/_24_swapPairs.java
+++ src/main/java/LeeCode/_24_swapPairs.java
@@ -13,13 +13,11 @@ public class _24_swapPairs {
 
     public static ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
-
         // 三个节点顺序:head, next, swapPairs(next.next)
         ListNode next = head.next;
         head.next = swapPairs(next.next);
         next.next = head;
         return next;
-
     }
 
 
---
+++ src/main/java/LeeCode/_26_removeDuplicates.java
@@ -0,0 +1,15 @@
+package LeeCode;
+
+public class _26_removeDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+    }
+}
