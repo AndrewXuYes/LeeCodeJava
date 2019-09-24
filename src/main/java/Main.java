3f13152ef6d07b7ffbb83bb7acc176e3330aa503
Andrew <522988922@qq.com>
2019-09-24 12:27:22 +0800

    EnumSingleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import org.apache.log4j.Logger;
+
+import java.util.Date;
+
 class Main {
-//    private static Logger logger = Logger.getLogger(Main.class);
+
+    private static Logger logger = Logger.getLogger(Main.class);
 
 
     public static void main(String[] args) {
 
-
-        System.out.println(3|9);
+        logger.info(new Date() + "   hello");
+        logger.error(new Date() +"   error");
     }
 }
