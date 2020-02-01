8037c92ad784205512984bb7fd76164ae6fd5d9e
Andrew <522988922@qq.com>
2020-02-01 19:47:09 +0800

    195 linux

---
+++ src/main/java/LeeCode/_195_Line10.java
@@ -0,0 +1,30 @@
+package LeeCode;
+
+public class _195_Line10 {
+    /**
+     * 给定一个文本文件 file.txt，请只打印这个文件中的第十行。
+     *
+     * 假设 file.txt 有如下内容：
+     *
+     * Line 1
+     * Line 2
+     * Line 3
+     * Line 4
+     * Line 5
+     * Line 6
+     * Line 7
+     * Line 8
+     * Line 9
+     * Line 10
+     * 你的脚本应当显示第十行：
+     *
+     * Line 10
+     *
+     */
+
+//    sed -n '10p' file.txt
+//    awk 'NR==10' file.txt
+//    tail -n +10 file.txt | head -1
+
+
+}
