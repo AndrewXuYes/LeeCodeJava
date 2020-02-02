6e5fbd29f1758414cc5f21e9d65571b554a3b219
Andrew <522988922@qq.com>
2020-02-02 10:49:53 +0800

    leecode

--- src/main/java/LeeCode/_215_findKthLargest.java
+++ src/main/java/LeeCode/_215_findKthLargest.java
@@ -1,6 +1,8 @@
 package LeeCode;
 
 
+import java.util.PriorityQueue;
+
 import static Utils.Sout.soutNum;
 
 public class _215_findKthLargest {
@@ -15,9 +17,25 @@ public class _215_findKthLargest {
     }
 
 
+    public static int findKthLargest2(int[] nums, int k) {
+        //定义PriorityQueue，默认从小到大
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> n1 - n2);
+        for (int n : nums) {
+            heap.add(n);
+            //思路是创建一个大顶堆，将所有数组中的元素加入堆中，并保持堆的大小小于等于 k。这样，堆中就保留了前 k 个最大的元素。
+            if (heap.size() > k)
+                heap.poll();
+        }
+        return heap.poll();
+    }
+
+
     public static void main(String[] args) {
         System.out.println(findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
         System.out.println(findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+        System.out.println();
+        System.out.println(findKthLargest2(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        System.out.println(findKthLargest2(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
 
     }
 }
