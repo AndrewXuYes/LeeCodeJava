01f3c1160d42bbe82df6f3fb7c825a141e82406a
Andrew <522988922@qq.com>
2020-02-07 00:03:03 +0800

    leecode

--- src/main/java/LeeCode/_66_plusOne.java
+++ src/main/java/LeeCode/_66_plusOne.java
@@ -7,9 +7,10 @@ public class _66_plusOne {
     public static int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             digits[i]++;
-            digits[i] = digits[i] % 10;
+            digits[i] %= 10;
             if (digits[i] != 0) return digits;
         }
+        //如果全部都不是9999再之前已经被返回走
         digits = new int[digits.length + 1];
         digits[0] = 1;
         return digits;
@@ -17,13 +18,7 @@ public class _66_plusOne {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3, 9})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 6, 9, 9})));
         System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9, 9})));
-
-
-        int[] aa = new int[]{1, 2, 3, 9};
-        aa = new int[5];
-        aa[0] = 1;
-        System.out.println(Arrays.toString(aa));
-
     }
 }
