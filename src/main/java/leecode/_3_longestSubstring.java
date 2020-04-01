3948970cd6061484a4d4e05c02399a2000b7e1d3
Andrew <522988922@qq.com>
2020-04-01 14:25:49 +0800

    1

--- src/main/java/leecode/_3_longestSubstring.java
+++ src/main/java/leecode/_3_longestSubstring.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class _3_longestSubstring {
+    /**
+     * 滑动窗口
+     *
+     */
 
     public static int lengthOfLongestSubstring(String s) {
         if (s.length() < 1) return 0;
@@ -21,21 +25,18 @@ public class _3_longestSubstring {
     }
 
     /**
-     * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetcod/
-     * <p>
-     * 上述的方法最多需要执行 2n 个步骤。事实上，它可以被进一步优化为仅需要 n 个步骤。我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。 当我们找到重复的字符时，我们可以立即跳过该窗口。
-     * <p>
+     * 上述的方法最多需要执行 2n 个步骤。 实上，它可以被进一步优化为仅需要 n 个步骤。
+     * 我们可以定义字符到索引的映射，而不是使用集合来判断一个字符是否存在。 当我们找到重复的字符时，我们可以立即跳过该窗口。
      * 也就是说，如果 s[j]s[j] 在 [i, j)[i,j) 范围内有与 j'j
      * ′
-     * 重复的字符，我们不需要逐渐增加 ii 。 我们可以直接跳过 [i，j'][i，j
-     * ′
-     * ] 范围内的所有元素，并将 ii 变为 j' + 1j +1。
+     * 重复的字符，我们不需要逐渐增加 ii 。 我们可以直接跳过 [i，j'][i，j'] 范围内的所有元素，并将 ii 变为 j' + 1j +1。
      */
     public static int lengthOfLongestSubstring22(String s) {
         int n = s.length(), max = 0;
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0, j = 0; j < n; j++) {
-            if (map.containsKey(s.charAt(j))) i = Math.max(map.get(s.charAt(j)), i);
+            if (map.containsKey(s.charAt(j)))
+                i = Math.max(map.get(s.charAt(j)), i);
             max = Math.max(max, j - i + 1);
             map.put(s.charAt(j), j + 1);
         }
