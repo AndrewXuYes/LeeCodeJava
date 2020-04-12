6df04ac36e327a1bc843a35c06d0b6d34e37533b
Andrew <522988922@qq.com>
2020-04-12 20:52:11 +0800

    1

--- src/main/java/leecode/_1_twoNumSum.java
+++ src/main/java/leecode/_1_twoNumSum.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class _1_twoNumSum {
 
     /**
-     *在进行迭代并将元素插入到表中的同时，
+     * 在进行迭代并将元素插入到表中的同时，
      * 检查表中是否已经存在当前元素所对应的目标元素。
      * 如果它存在，那我们已经找到了对应解，并立即将其返回。
      */
@@ -16,9 +16,8 @@ public class _1_twoNumSum {
         Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < a.length; i++) {
             System.out.println(i);
-            if (hashMap.containsKey(b - a[i])) {
+            if (hashMap.containsKey(b - a[i]))
                 return new int[]{hashMap.get(b - a[i]), i};
-            }
             hashMap.put(a[i], i);
         }
         return new int[]{-1, -1};
